fcf05c1a25727f9505190be753b0c12767cda553
furPedroRibeiro <pedrocodejob@gmail.com>
2024-01-17 23:50:35 -0300

    first commit, utilizando bibliotecas

---
+++ src/script.js
@@ -0,0 +1,12 @@
+const myAtropos = Atropos({
+    el: '.my-atropos',
+    // rest of parameters
+});
+const mySecondAtropos = Atropos({
+    el: '.my-second-atropos',
+    // rest of parameters
+});
+const myThirdAtropos = Atropos({
+    el: '.my-third-atropos',
+    // rest of parameters
+});
